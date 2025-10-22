33ae53baf9b6af2cfac623ec1db412d3d42b27f4
oliviaSerraa <oserra@pioix.edu.ar>
2025-10-22 08:42:45 -0300

    22/10

--- mi-proyecto-nextjs/front/src/components/Anotador.js
+++ mi-proyecto-nextjs/front/src/components/Anotador.js
@@ -3,37 +3,21 @@
 import React from "react"
 
 export default function Anotador (props) {
-    <>
+
+    const sospechosos = ["Señorita Escarlata", "Señora Azulino", "Profesor Moradillo", "Señor Verdi", "Señora Blanco"]
+    const armas = ["Cuchillo", "Revólver", "Soga", "Llave inglesa", "Veneno"]
+    const habitaciones = ["Habitación", "Comedor", "Cocina", "Baño"]
+
+    /*sospechosos.map( () => (sospechoso, index) {
+        <p>{sospechoso}</p>
+        <input key={index} type="checkbox"></input>
+    })*/
+
+    return<>
         <h2>sospechosos</h2>
         <p>Señorita Escarlata</p>
         <select type="checkbox"></select>
-        <p>Señora Azulino</p>
-        <select type="checkbox"></select>
-        <p>Profesor Moradillo</p>
-        <select type="checkbox"></select>
-        <p>Señor Verdi</p>
-        <select type="checkbox"></select>
-        <p>Señora Blanco</p>
-        <select type="checkbox"></select>
         <h2>armas</h2>
-        <p>Cuchillo</p>
-        <select type="checkbox"></select>
-        <p>Revólver</p>
-        <select type="checkbox"></select>
-        <p>Soga</p>
-        <select type="checkbox"></select>
-        <p>Llave inglesa</p>
-        <select type="checkbox"></select>
-        <p>Veneno</p>
-        <select type="checkbox"></select>
         <h2>habitaciones</h2>
-        <p>Habitación</p>
-        <select type="checkbox"></select>
-        <p>Comedor</p>
-        <select type="checkbox"></select>
-        <p>Cocina</p>
-        <select type="checkbox"></select>
-        <p>Baño</p>
-        <select type="checkbox"></select>
     </>
 }
