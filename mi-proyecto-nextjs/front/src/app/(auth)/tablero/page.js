6598ed4881178247369be3344704e05afa51b779
camilagiadini <cgiardini@pioix.edu.ar>
2025-10-29 11:13:03 -0300

    29/10

--- mi-proyecto-nextjs/front/src/app/(auth)/tablero/page.js
+++ mi-proyecto-nextjs/front/src/app/(auth)/tablero/page.js
@@ -8,21 +8,28 @@ import styles from "./page.module.css";
 import clsx from 'clsx';
 
 export default function Tablero() {
-    const [users, setUsers] = useState([])
+    const [usersInRoom, setUsersInRoom] = useState([])
+    //const [numeroObtenido, setNumeroObtenido] = useState(0)
+    let numeroObtenido=0
 
-    async function obtenerUsuarios() {
-        fetch ("http://localhost:4000/users")
-        .then(response => response.json())
-        .then(result => {
-            setUsers(result)
-        })
+    function getRandomIntInclusive(min, max) {
+        min = Math.ceil(min)
+        max = Math.floor(max)
+        return Math.floor(Math.random() * (max - min + 1)) + min
     }
+
+    function obtenerNumeroAleatorio () {
+        numeroObtenido = getRandomIntInclusive(1, 6)
+        console.log(numeroObtenido)
+    }
+
 
     return (
         <>
             <div className={styles["pagina-tablero"]}>
                 <Anotador></Anotador>
                 <Grilla></Grilla>
+                <button onClick={obtenerNumeroAleatorio}>aaaa</button>
             </div>
         </>
     )
